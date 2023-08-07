a597fec1e1268d1426b71b3d1ce9d9c2e2cfb2c4
Deniz Pancar <deniz010507@gmail.com>
2023-08-07 22:54:02 +0300

    Finished UI added plane game

--- UI/main.py
+++ UI/main.py
@@ -24,18 +24,19 @@ def get_scaling():
     return scaling
 
 buttonColor = "#62acb5"
+backgrounColor = "#989898"
 
 pad1 = 85
 pad2 = 25
 
 layout = [
-    [sg.Text("Eye and Gesture Tracking", pad=(pad1*.5, pad2), background_color='#989898'), sg.Button("Guidelines", pad=(pad1*.65, pad2), size=(9, 2), button_color=buttonColor)],
+    [sg.Text("Eye and Gesture Tracking", pad=(pad1*.5, pad2), background_color=backgrounColor), sg.Button("Guidelines", pad=(pad1*.65, pad2), size=(9, 2), button_color=buttonColor)],
     [sg.Button("Random Game", pad=(pad1*.9, pad2), button_color=buttonColor), sg.Button("Plane Game", pad=(pad1*2.9, pad2), size=(10, 1), button_color=buttonColor)],
     [sg.Button("Pong", pad=(pad1*1, pad2), size=(10, 1), button_color=buttonColor),sg.Button("Rock Paper Scissors", pad=(pad1*3, pad2), size=(13, 1), button_color=buttonColor)],
     [sg.Button("Connect 4", pad=(pad1*1, pad2), size=(10, 1), button_color=buttonColor),sg.Button("Memory Game", pad=(pad1*3, pad2), size=(12, 1), button_color=buttonColor)]
 ]
 
-window = sg.Window("Eye and Gesture Tracking", layout, resizable=True, margins=(0, 0), background_color='#989898').finalize()
+window = sg.Window("Eye and Gesture Tracking", layout, resizable=True, margins=(0, 0), background_color=backgrounColor).finalize()
 my_scaling = get_scaling()      # call get_scaling()
 my_width, my_height = window.get_screen_size()  # call sg.Window.get_screen_size()
 window.maximize()
@@ -45,15 +46,15 @@ gameLaunches = {
     "Connect 4":"Connect 4.py",
     "Rock Paper Scissors":"rock_paper_scissors.py",
     "Memory Game":"Memory_Game_Integrated.py",
-    "Plane Game":None
+    "Plane Game":"plane_game.py"
 }
 
 
 
 def openGuidelines():
-    column = [[sg.Image(filename='guidelines.png', key='Image',background_color='#989898')]]
-    layout = [[sg.Column(column, size=(1500, 860), scrollable=True, key='Column',background_color='#989898')]]
-    window = sg.Window('test', layout, finalize=True, background_color='#989898')
+    column = [[sg.Image(filename='guidelines.png', key='Image',background_color=backgrounColor)]]
+    layout = [[sg.Column(column, size=(1500, 860), scrollable=True, key='Column',background_color=backgrounColor)]]
+    window = sg.Window('test', layout, finalize=True, background_color=backgrounColor)
     window.maximize()
 
     while True:
@@ -72,19 +73,25 @@ while True:
         gamePath = gameLaunches[event]
         if gamePath:
             with open(path / gamePath) as f:
-                exec(f.read())
+                try:
+                    exec(f.read())
+                except:
+                    pass
             break
         else:
-            sg.popup_error("Not yet implemented")
+            sg.popup_error("Not yet implemented",background_color=backgrounColor)
     
     if event == "Random Game":
         gamePath = choice(list(gameLaunches.values()))
         if gamePath:
             with open(path / gamePath) as f:
-                exec(f.read())
+                try:
+                    exec(f.read())
+                except:
+                    pass
             break
         else:
-            sg.popup_error("Not yet implemented")
+            sg.popup_error("Not yet implemented",background_color=backgrounColor)
     
     if event == "Guidelines":
         openGuidelines()
